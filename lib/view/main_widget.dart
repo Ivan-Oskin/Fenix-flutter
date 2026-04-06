0df2091256388c599ad801d3c69cf6a069e0e1ec
Ivan-Oskin <Serivanoskin123@gmail.com>
2026-04-06 22:26:39 +0300

    Сделал кнопочки

--- lib/view/main_widget.dart
+++ lib/view/main_widget.dart
@@ -34,8 +34,87 @@ class MainWidget extends StatelessWidget {
     ),
   );
 
+  late final scanner = Container(
+    width: 125,
+    height: 125,
+    decoration: BoxDecoration(
+      color: Color(0xFFD9D9D9),
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: Column(
+      children: [
+        Center(
+          child: Image.asset(
+            'assets/images/main_widget/qr_code.png',
+            width: 90,
+            height: 90,
+          ),
+        ),
+        Text("Сканировать QR", style: TextStyle(color: Color(0xBF484C52))),
+      ],
+    ),
+  );
+
+  late final buttonIdBox = SizedBox(
+    width: 105,
+    height: 90,
+    child: Column(
+      children: [
+        Container(
+          width: 82,
+          height: 70,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: Color(0xFFD9D9D9), width: 2),
+          ),
+          child: Image.asset('assets/images/main_widget/ID.png'),
+        ),
+        Text("Войти по ID", style: TextStyle(color: Color(0xBF484C52))),
+      ],
+    ),
+  );
+
+  late final buttonFindBox = SizedBox(
+    width: 100,
+    height: 90,
+    child: Column(
+      children: [
+        Container(
+          width: 82,
+          height: 70,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: Color(0xFFD9D9D9), width: 2),
+          ),
+          child: Image.asset('assets/images/main_widget/loupe.png'),
+        ),
+        Text("Найти встречу", style: TextStyle(color: Color(0xBF484C52))),
+      ],
+    ),
+  );
+
+  late final buttons = SizedBox(
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [buttonIdBox, buttonFindBox],
+    ),
+  );
+
+  late final qrBlock = Align(
+    alignment: Alignment.topCenter,
+    child: SizedBox(
+      width: 335,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [scanner, buttons],
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: welcomeBlock));
+    return MaterialApp(
+      home: Scaffold(body: Column(children: [welcomeBlock, qrBlock])),
+    );
   }
 }
